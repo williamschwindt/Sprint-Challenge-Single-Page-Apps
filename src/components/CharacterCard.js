89c4999af56e823e5e1aa05b542e9407a9f41d8e
William Schwindt <williammschwindt@gmail.com>
2020-01-26 17:32:29 -0800

    characters fetched, routes set up, search in progress

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({ character }) {
+  return (
+    <div>
+      <h2>{character.name}</h2>
+      <img src={character.image} alt={character.name} />
+    </div>
+  );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import CharacterCard from "./CharacterCard";
 
-export default function SearchForm() {
- 
+export default function SearchForm({ characters }) {
+  const [filteredItems, setFilteredItems] = useState([]);
+
+  const handleChange = (e) => {
+    let currentCharacters = [];
+    let newCharacters = [];
+
+    if (e.target.value !== "") {
+      newCharacters = filteredItems.filter(person => {
+        const lowerCase = person.toLowerCase();
+        const filter = e.target.value.toLowerCase();
+        return lowerCase(filter);
+      });
+
+      setFilteredItems(newCharacters);
+    }
+  };
+
+  const search = (e) => {
+    e.preventDefault();
+  };
+
+
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <form>
+        <input placeholder="search" type="text" name="search" onChange={handleChange} ></input>
+        <button type="submit" onClick={search} >Submit</button>
+      </form>
+      {
+        filteredItems.map(character => {
+          return <CharacterCard key={character.id} character={character} />
+        })
+      }
     </section>
   );
 }
